4c60d36ef002706216c5e6cd2f2b0498bbf56b82
Eric Zhou <42453465+TheZooo@users.noreply.github.com>
2019-02-26 12:55:57 -1000

    Update gameScript.js

--- gameScript.js
+++ gameScript.js
@@ -1,85 +1,137 @@
 var w = window.innerWidth; //window width subtracted by border size
 var h = window.innerHeight; //window height subtracted by border size
-var startAmount = 2;
-var ball0 = new ball0(); //creates global object ballA
-var ball1 = new ball1(); //creates global object ballB
-var ballSize = 50; //Global ball size
+var ballA = new ballA(); //creates global object ballA
+var ballB = new ballB(); //creates global object ballB
+var ballSizeA = 30; //Global ballA size
+var ballSizeB = 30; //Global ballB size
+var baseBallSpeedA = 5; //To reset speedA
+var ballSpeedA = baseBallSpeedA; //Set speed of ballA
+var ballSprintA = 8; //Sprint speed
+var stamA = 80; //Stamina for sprinting
+var stamMaxA = 60; //Max staminaA
+var baseBallSpeedB = 5; //To reset speedB
+var ballSpeedB = baseBallSpeedB; //Set speed of ballB
+var ballSprintB = 8; //Sprint speed
+var stamB = 80; //Stamina for sprinting
+var stamMaxB = 60; //Max staminaB
+var d; //Distance
+var tacked = ["BOPPED", "TACKLED", "TRAMPLED", "DOMINATED", "CAUGHT", "PUNCHED"]; //Wow
 
 function setup() {
   createCanvas(w, h);
 }
 
 function draw() {
-  background(255);
-  ball0.show();
-  ball0.move();
-  ball1.show();
-  ball1.move();
+  background(220);
+  line(ballA.x,ballA.y,ballB.x,ballB.y);
+  d = Math.floor(dist(ballA.x,ballA.y,ballB.x,ballB.y));
+  if (d < ballSizeA) {
+    collision();
+    noLoop();
+  }
+  ballA.show();
+  ballA.move();
+  ballB.show();
+  ballB.move();
+  document.getElementById('dis').innerHTML = "Distance of line: " + d + " ..." + " Controls: Player1: WASD and shift ... Player2: IJKL and space";
+  document.getElementById('playA').innerHTML = "Player1 stamina: " + stamA;
+  document.getElementById('playB').innerHTML = "Player2 stamina: " + stamB;
 }
 
-function ball0() { //Global object "ball"
-  this.x = 100;
-  this.y = 100;
+function ballA() { //Global object "ball"
+  this.x = 50;
+  this.y = 50;
   this.show = function() { //Created function "ballA.show()"
     fill(color('red'));
-    ellipse(this.x,this.y,ballSize,ballSize);
-    document.getElementById('dis').innerHTML = "x: " + this.x + " y: " + this.y;
+    ellipse(this.x,this.y,ballSizeA,ballSizeA);
   };
   this.move = function() { //Created function "ballA.move()"
     if (keyIsDown(65)) { //Left
-      this.x -= 10;
+      this.x -= ballSpeedA;
     }
     if (keyIsDown(68)) { //Right
-      this.x += 10;
+      this.x += ballSpeedA;
     }
     if (keyIsDown(87)) { //Up
-      this.y -= 10;
+      this.y -= ballSpeedA;
     }
     if (keyIsDown(83)) { //Down
-      this.y += 10;
+      this.y += ballSpeedA;
     }
-    if (this.x < ballSize) { //Border Left
-      this.x = ballSize;
-    } else if (this.x > w - ballSize) { //Border Right
-      this.x = w - ballSize;
+    if (keyIsDown(16)) { //Speed up for A
+      if (stamA > 0) {
+        stamA--;
+        ballSpeedA = ballSprintA;
+      }
+    } else {
+      if (stamA < stamMaxA) {
+        stamA++;
+      }
     }
-    if (this.y < ballSize) { //Border Up
-      this.y = ballSize;
-    } else if (this.y > h - ballSize) { //Border Down
-      this.y = h - ballSize;
+    if (stamA === 0) {
+      ballSpeedA = baseBallSpeedA;
+    }
+    if (this.x < ballSizeA) { //Border Left
+      this.x = ballSizeA;
+    } else if (this.x > w - ballSizeA) { //Border Right
+      this.x = w - ballSizeA;
+    }
+    if (this.y < ballSizeA) { //Border Up
+      this.y = ballSizeA;
+    } else if (this.y > h - ballSizeA) { //Border Down
+      this.y = h - ballSizeA;
     }
   };
 }
 
-function ball1() { //Player 2 ballB()
-  this.x = 200;
-  this.y = 200;
+function ballB() { //Player 2 ballB()
+  this.x = w;
+  this.y = h;
   this.show = function() {
     fill(color('blue'));
-    ellipse(this.x,this.y,ballSize,ballSize);
+    ellipse(this.x,this.y,ballSizeB,ballSizeB);
   };
   this.move = function() {
     if (keyIsDown(74)) { //Left
-      this.x -= 10;
+      this.x -= ballSpeedB;
     }
     if (keyIsDown(76)) { //Right
-      this.x += 10;
+      this.x += ballSpeedB;
     }
     if (keyIsDown(73)) { //Up
-      this.y -= 10;
+      this.y -= ballSpeedB;
     }
     if (keyIsDown(75)) { //Down
-      this.y += 10;
+      this.y += ballSpeedB;
     }
-    if (this.x < ballSize) { //Border Left
-      this.x = ballSize;
-    } else if (this.x > w - ballSize) { //Border Right
-      this.x = w - ballSize;
+    if (keyIsDown(32)) {
+      if (stamB > 0) {
+        stamB--;
+        ballSpeedB = ballSprintB;
+      }
+    } else {
+      if (stamB < stamMaxB) {
+        stamB++;
+      }
     }
-    if (this.y < ballSize) { //Border Up
-      this.y = ballSize;
-    } else if (this.y > h - ballSize) { //Border Down
-      this.y = h - ballSize;
+    if (stamB === 0) {
+      ballSpeedB = baseBallSpeedB;
+    }
+    if (this.x < ballSizeB) { //Border Left
+      this.x = ballSizeB;
+    } else if (this.x > w - ballSizeB) { //Border Right
+      this.x = w - ballSizeB;
+    }
+    if (this.y < ballSizeB) { //Border Up
+      this.y = ballSizeB;
+    } else if (this.y > h - ballSizeB) { //Border Down
+      this.y = h - ballSizeB;
     }
   };
+}
+
+function collision() {
+  var rand = Math.floor(Math.random() * tacked.length);
+  window.alert(tacked[rand]);
+  location.href = "starterGame.html";
 }
